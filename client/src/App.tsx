0807abff58b5b4143d17f8bd2a64d6971ee7532e
Ankush Roy <imankushroy@gmail.com>
2024-09-13 02:22:24 +0530

    client build added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,7 @@ import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 export const BASE_API_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5000/api"
-    : "deployed.server.url/api";
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 
 function App() {
   return (
